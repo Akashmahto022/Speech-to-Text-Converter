3920d7fe339b1ae7e0a6de366f4dd80dc856acb4
Akash kumar <akashmahto1169@gmail.com>
2023-09-10 12:18:39 +0530

    Speech to Text Converter app completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
 
 function App() {
+  const [copyText, setCopyText] = useState()
+  const [isCopied, setCopied] = useClipboard(copyText);
+
+  const startListeing = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="container">
+        <h2>Speech to <span>Text Converter</span></h2>
+        <br />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A speech from the microphone to text
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p><span>To copy the text first click on the text area and then click on the copt the text.</span></p>
+
+        <div className="main-content" onClick={()=> setCopyText(transcript)}>{transcript}</div>
+
+        <div className="btn-style">
+          <button onClick={setCopied}>
+            Copy the Text ? {isCopied ? "Yes! 👍" : "Nope! 👎"}
+          </button>
+          <button onClick={startListeing}>Start Listening</button>
+          <button onClick={SpeechRecognition.stopListening}>
+            Stop Listening
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
